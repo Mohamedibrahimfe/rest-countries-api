7c0a2fc10975edc5f21d86a8c90c529970be796a
Mohamedibrahimfe <147580141+Mohamedibrahimfe@users.noreply.github.com>
2024-10-10 16:54:46 +0300

    all functionalities work
    need to redirect with click

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   const [input, setInput] = useState("");
   const [search, setSearch] = useState(null);
   const [allCountries, setAllCountries] = useState([]);
-
+  const [region, setRegion] = useState("");
   const getCountries = async () => {
     const response = await fetch(`https://restcountries.com/v3.1/all`);
     const data = await response.json();
@@ -31,25 +31,69 @@ function App() {
   useEffect(() => {
     getCountries();
   }, []);
+  const getCountriesByFilter = async () => {
+    const response = await fetch(
+      `https://restcountries.com/v3.1/region/${region}`
+    );
+    const data = await response.json();
+    setAllCountries(data);
+  };
 
+  useEffect(() => {
+    if (region) {
+      getCountriesByFilter();
+    } else {
+      getCountries();
+    }
+  }, [region]);
+  // console.log(allCountries);
   return (
     <div className="App">
       <Navbar />
       <div className="container">
         <div className="search">
-          <div>
-            {" "}
+          <div className="search-box">
+            <svg
+              width="167px"
+              height="167px"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              onClick={() => getSearch()}
+              className="search-icon"
+            >
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                {" "}
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M10 5C7.23858 5 5 7.23858 5 10C5 12.7614 7.23858 15 10 15C11.381 15 12.6296 14.4415 13.5355 13.5355C14.4415 12.6296 15 11.381 15 10C15 7.23858 12.7614 5 10 5ZM3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10C17 11.5719 16.481 13.0239 15.6063 14.1921L20.7071 19.2929C21.0976 19.6834 21.0976 20.3166 20.7071 20.7071C20.3166 21.0976 19.6834 21.0976 19.2929 20.7071L14.1921 15.6063C13.0239 16.481 11.5719 17 10 17C6.13401 17 3 13.866 3 10Z"
+                  fill="#000000"
+                ></path>{" "}
+              </g>
+            </svg>{" "}
             <input
               type="text"
               value={input}
               placeholder="Search for a country..."
               onChange={(e) => setInput(e.target.value)}
             ></input>
-            <i className="fa-solid fa-magnifying-glass" onClick={getSearch}></i>
           </div>
 
           <div className="filter">
-            <select name="region" id="region">
+            <select
+              onChange={(e) => {
+                setRegion(e.target.value);
+              }}
+              name="region"
+              id="region"
+            >
               <option value="Africa">Africa</option>
               <option value="Americas">Americas</option>
               <option value="Asia">Asia</option>
@@ -65,9 +109,9 @@ function App() {
                 <img src={country.flags.png} alt={country.name.common} />
                 <div className="info">
                   <h1>{country.name.common}</h1>
-                  <p>{country.population}</p>
-                  <p>{country.region}</p>
-                  <p>{country.capital}</p>
+                  <p>population: {country.population}</p>
+                  <p>Region: {country.region}</p>
+                  <p>capital: {country.capital}</p>
                 </div>
               </div>
             );
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,10 +1,33 @@
 const Navbar = () => {
-    return ( 
-        <nav className="navbar">
-            <h1>Where in the world ?</h1>
-            <div><i className=""/> <span>Dark Mode</span></div>
-        </nav>
-     );
+    return (
+      <nav className="navbar">
+        <h1>Where in the world ?</h1>
+        <div
+          className="toggle"
+          onClick={() => document.body.classList.toggle("light")}
+        >
+          <svg
+            fill="#ffffff"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            id="moon-alt-7"
+            className="icon glyph"
+            transform="rotate(-45)matrix(-1, 0, 0, 1, 0, 0)"
+          >
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <path d="M5,21.21A10,10,0,1,0,5,2.79,10,10,0,0,1,5,21.21Z"></path>
+            </g>
+          </svg>
+          <span>Dark Mode</span>
+        </div>
+      </nav>
+    );
 }
  
 export default Navbar;
